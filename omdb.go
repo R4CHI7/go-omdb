1862e2742c147babb321fc3533a2b41cfda5fd41
Rachit Gupta <rachitgupta1792@gmail.com>
2016-09-10 23:26:12 +0530

    Added search by title wrapper and unit test.

---
+++ omdb.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+var baseUrl string = "http://www.omdbapi.com/"
+
+type SingleResultResponse struct {
+	Title      string `json:"Title"`
+	Year       string `json:"Year"`
+	Rated      string `json:"Rated"`
+	Released   string `json:"Released"`
+	Runtime    string `json:"Runtime"`
+	Genre      string `json:"Genre"`
+	Director   string `json:"Director"`
+	Writer     string `json:"Writer"`
+	Actors     string `json:"Actors"`
+	Plot       string `json:"Plot"`
+	Language   string `json:"Language"`
+	Country    string `json:"Country"`
+	Awards     string `json:"Awards"`
+	Poster     string `json:"Poster"`
+	Metascore  string `json:"Metascore"`
+	ImdbRating string `json:"imdbRating"`
+	ImdbVotes  string `json:"imdbVotes"`
+	ImdbId     string `json:"imdbId"`
+	Type       string `json:"Type"`
+	Response   string `json:"Response"`
+	Error      string `json:"Error"`
+}
+
+func SearchByTitle(searchTerm string) (SingleResultResponse, error) {
+	req, err := http.NewRequest("GET", baseUrl, nil)
+	if err != nil {
+		return SingleResultResponse{}, err
+	}
+
+	query := req.URL.Query()
+	query.Add("t", searchTerm)
+	req.URL.RawQuery = query.Encode()
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return SingleResultResponse{}, err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return SingleResultResponse{}, err
+	}
+
+	var response SingleResultResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return SingleResultResponse{}, err
+	}
+
+	return response, nil
+}
---
+++ omdb_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSearchByTitle(t *testing.T) {
+	searchTerm := "Into The Wild"
+	_, err := SearchByTitle(searchTerm)
+	if err != nil {
+		t.Errorf("Expected error to be nil. Got: %s", err.Error())
+	}
+}
